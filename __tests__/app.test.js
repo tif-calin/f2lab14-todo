e84c488bc0519d5d06c3bc710fd1356062efacee
tif-calin <culitif@tuta.io>
2021-05-13 17:11:06 -0700

    write test for GET /api/me/todos

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,6 +13,7 @@ describe('API Routes', () => {
 
   describe('/api/todos', () => {
     let user;
+    let user2;
 
     beforeAll(async () => {
       execSync('npm run recreate-tables');
@@ -28,6 +29,18 @@ describe('API Routes', () => {
       expect(response.status).toBe(200);
 
       user = response.body;
+
+      const response2 = await request
+        .post('/api/auth/signup')
+        .send({
+          name: 'Other User',
+          email: 'other@user.com',
+          password: 'notpassword'
+        });
+      
+      expect(response2.status).toBe(200);
+      
+      user2 = response.body;
       
     });
 
@@ -43,21 +56,57 @@ describe('API Routes', () => {
         userId: user.id
       };
 
-     
-
       const task = {
         task: 'walk the cats',
         completed: false,
         shared: false
       };
 
-      const response =  await request.post('/api/todos').set('Authorization', user.token).send(task);
-      
-      
+      const response = await request.post('/api/todos').set('Authorization', user.token).send(task);
     
       expect(response.status).toBe(200);
       expect(response.body).toEqual(expected);
       
+    });
+
+    it('GET /api/me/todos get all todos of current user only', async () => {
+
+      // make two test tasks
+      const task1 = {
+        task: 'walk the cats',
+        completed: false,
+        shared: true
+      };
+      const task2 = {
+        task: 'wash the grapefruit',
+        completed: true,
+        shared: false
+      };
+
+      // push 2 tasks from 2 different users
+      const response1 = await request
+        .post('/api/todos')
+        .set('Authorization', user.token)
+        .send(task1);
+      const response2 = await request
+        .post('/api/todos')
+        .set('Authorization', user2.token)
+        .send(task2);
+      
+      // test the post responses
+      expect(response1.status).toBe(200);
+      expect(response2.status).toBe(200);
+
+      // get list of todos from first user only
+      const response = await request
+        .get('/api/me/todos')
+        .set('Authorization', user.token);
+      
+      // test to make sure only from this user
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(expect.not.arrayContaining([response2.body]));
+      expect(response.body).toEqual([response1.body]);
+
     });
 
   });
